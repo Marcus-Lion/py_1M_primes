a2cc0649274a4100c27cfd2587e25f046a514578
Marc Deveaux <mdvx@users.noreply.github.com>
2024-08-21 02:43:07 -0400

    primes

--- main.py
+++ main.py
@@ -2,14 +2,20 @@ def calc_100m_primes(n: int):
     is_prime = [False, False] + [True] * (n - 1)
     primes = [2]
 
+    print('initializing...')
     for j in range(4, n + 1, 2):
         is_prime[j] = False
+
+    print('initialized')
 
     for i in range(3, n + 1, 2):
         if is_prime[i]:
             primes.append(i)
             for j in range(i * i, n + 1, i):
                 is_prime[j] = False
+
+            if len(primes) % 10000 == 0:
+                print(f"primes found {len(primes)}")
 
     print(primes[-10:])
     return len(primes)
