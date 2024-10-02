b805d6196b94e60c9bff60119be0bdca823cd9cb
Marc Deveaux <mdvx@users.noreply.github.com>
2024-10-01 23:22:49 -0400

    refactor tests

---
+++ Primes25k.py
@@ -0,0 +1,31 @@
+def is_prime(n: int, primes: [int]) -> bool:
+    # if n < 2:
+    #     return False
+    #
+    for i in primes[1:]:
+        if n % i == 0:
+            return False
+
+    return True
+
+def calc_n_primes(n: int):
+    print('initializing...')
+
+    primes = [2]
+
+    i = 3
+    while len(primes) < n:
+        if is_prime(i, primes):
+            primes.append(i)
+            if (len(primes) % 100) == 0:
+                print(f"prime # {len(primes)} found {i}")
+
+        i += 2
+
+    print(primes[-10:])
+    return len(primes)
+
+
+print(f'ml test {__file__}')
+num = calc_n_primes(25_000)
+print(f"primes found {num}")
